52b33184afc933dd6aa237a34c2d44ba7b34f257
Jakub Kilar <kuba.kilar@gmail.com>
2018-05-27 16:38:33 +0200

    create component

---
+++ src/CommentsListContainer.js
@@ -0,0 +1,8 @@
+import { connect } from 'react-redux';
+import CommentsList from './CommentsList';
+
+const mapStateToProps = state => ({
+  comments: state.comments
+});
+/* wywołujemy funkcję connect, która zwraca funkcję. Następnie, wywołanie tej funkcji dopiero podpina się pod komponent CommentsList. ??? */
+export default connect(mapStateToProps)(CommentsList);
