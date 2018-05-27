78737fd381d9638060972e77eda32d729a9394f3
Jakub Kilar <kuba.kilar@gmail.com>
2018-05-27 17:13:11 +0200

    add imports and dispatch

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// https://redux.js.org/basics/example-todo-list import render?
+import { render } from 'react-dom'
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducer from './reducers';
+
+const store = createStore(reducer);
 
 ReactDOM.render(
   <Provider store={store}>
@@ -12,3 +18,6 @@ ReactDOM.render(
   document.getElementById('root')
 );
 registerServiceWorker();
+
+store.dispatch(addComment('pierwszy komentarz'));
+store.dispatch(addComment('drugi komentarz'));
