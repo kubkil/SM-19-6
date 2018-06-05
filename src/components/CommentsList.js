78d88dc82953205dfa8d22845aa1c1d894fca8c6
Jakub Kilar <kuba.kilar@gmail.com>
2018-06-05 19:13:31 +0200

    create comments list container

--- src/components/CommentsList.js
+++ src/components/CommentsList.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Comment from './Comment';
 import CommentContainer from '../containers/CommentContainer';
 // ...comment = comment.votes comment.id  comment trafia z CommentsListContainer.js
-const CommentsList = ({ comments }) => 
+const CommentsList = ({ comments }) =>
   <ul>
     {comments.map(comment => <CommentContainer key={comment.id} {...comment} />)}
   </ul>;
