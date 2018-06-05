4cf6e5e10f098d975ffbc739c9c9ac0e205db96a
Jakub Kilar <kuba.kilar@gmail.com>
2018-06-05 19:13:50 +0200

    refactor

--- src/containers/AddCommentContainer.js
+++ src/containers/AddCommentContainer.js
@@ -1,9 +1,10 @@
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Comment from '../components/Comment';
 import { addComment } from '../actions/actions';
 import AddComment from '../components/AddComment';
 
-
+// statetoprops/ dispatchtoprops?
 const mapDispatchToProps = dispatch => ({
   addComment: (text) => dispatch(addComment(text))
 });
