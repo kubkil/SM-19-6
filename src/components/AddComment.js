73898e32e0f371efd3da62f26f5ad8bae2d40fff
Jakub Kilar <kuba.kilar@gmail.com>
2018-06-05 19:12:26 +0200

    create AddComment container

--- src/components/AddComment.js
+++ src/components/AddComment.js
@@ -1,12 +1,42 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import {addComment} from '../actions/actions';
 
-const AddComment = ({text, addComment}) => {
+const AddComment = ({dispatch}) => {
+  let input;
+
   return (
-    <form>
-      <input type='text' placeholder='type comment' value={text}/>
-      <button type='submit' onClick={() => addComment(text)}>Post</button>
+    <form onSubmit={e => {
+            e.preventDefault();
+            dispatch(addComment(input.value));
+            input.value ='';}}>
+      <input type='text' ref={node => input = node} />
+      <button type='submit'>POST</button>
     </form>
-  )
+  );
 }
 
-export default AddComment;
+export default connect()(AddComment);
+
+// const AddComment = (props) => (
+//   <form onSubmit={(e) => {
+//     e.preventDefault();
+//     props.onCommentSubmit();
+//     }}>
+
+//     <input type='text' placeholder='type comment' value={props.text} onChange={(e) => props.onComment(e.target.value)} />
+//     {/* type potrzebne? */}
+//     <button type='submit'>POST</button>
+//   </form>
+// )
+
+// const AddComment = ({text, addComment}) => {
+//   return (
+//     <form>
+//       <input type='text' placeholder='type comment' value={text}/>
+//       <button type='submit' onClick={() => addComment({text})}>Post</button>
+//     </form>
+//   )
+// }
+
+// export default AddComment;
