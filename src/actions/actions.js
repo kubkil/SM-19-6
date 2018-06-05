5e7558b47008ea2f8cee556421e4f7f3ee88d1c1
Jakub Kilar <kuba.kilar@gmail.com>
2018-06-05 19:11:59 +0200

    refactor

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -5,6 +5,7 @@ export const REMOVE_COMMENT = 'REMOVE_COMMENT';
 export const THUMB_UP = 'THUMB_UP';
 export const THUMB_DOWN = 'THUMB_DOWN';
 
+// czy powinno być id?
 export const addComment = text => ({
   type: ADD_COMMENT,
   id: uuid.v4(),
